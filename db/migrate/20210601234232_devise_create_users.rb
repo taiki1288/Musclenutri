4b576c3658b5560554947428c90834ba81a3e43d
taiki yamashita <taiki0808ty@gmail.com>
2021-06-02 12:12:04 +0900

    [add] Bootstrap install

--- db/migrate/20210601234232_devise_create_users.rb
+++ db/migrate/20210601234232_devise_create_users.rb
@@ -8,7 +8,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ''
       t.string :username, null: false, default: ''
 
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
