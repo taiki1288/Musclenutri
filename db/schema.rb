9c2f00e34f8774d50f1579a1d32ad5848191f922
taiki yamashita <taiki0808ty@gmail.com>
2021-06-03 16:20:20 +0900

    [bug] devise sign inが反応しない

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_06_01_234232) do
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username", default: "", null: false
