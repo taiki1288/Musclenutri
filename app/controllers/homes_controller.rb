5fb3cbb77553feb459041f309cbe214f6585fc64
taiki yamashita <taiki0808ty@gmail.com>
2021-06-03 23:47:35 +0900

    [update] guest login

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,14 +2,4 @@ class HomesController < ApplicationController
     def index
 
     end
-
-    def new_guest
-        user = User.find_or_create_by(email: 'guest@example.com') do |user|
-            user.password = SecureRandom.urlsafe_base64
-            user.username = 'guest'
-        end
-
-        sign_in user
-        redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
-
-  post '/homes/guest_sign_in', to: 'homes#new_guest'
+  devise_scope :user do
+    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
