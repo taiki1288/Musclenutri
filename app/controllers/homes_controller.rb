e5601022c82417cc6c3e8992dc73fdc0a3833781
taiki yamashita <taiki0808ty@gmail.com>
2021-06-03 23:06:52 +0900

    [add] guest login

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,15 @@
 class HomesController < ApplicationController
     def index
-        
+
+    end
+
+    def new_guest
+        user = User.find_or_create_by(email: 'guest@example.com') do |user|
+            user.password = SecureRandom.urlsafe_base64
+            user.username = 'guest'
+        end
+
+        sign_in user
+        redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
     end
 end
