a147435cb636b8bc22f34a7129f609dc5e62164f
taiki yamashita <taiki0808ty@gmail.com>
2021-06-05 19:54:50 +0900

    feat: profile controller action(edit, update)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,26 @@
 class ProfilesController < ApplicationController
     before_action :authenticate_user!
 
-    def show  
+    def show
         @profile = current_user.profile
     end
 
     def edit
+        @profile = current_user.build_profile
+    end
+
+    def update
+        @profile = current_user.build_profile(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィールを更新しました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(:nickname, :introduction, :avatar)
     end
 end
