dc4a8b6366c2f625fdeb616c89ef694e9fcecef1
taiki yamashita <taiki0808ty@gmail.com>
2021-06-06 13:48:01 +0900

    feat: post action [update]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,15 @@ class PostsController < ApplicationController
         @post = current_user.posts.find(params[:id])
     end
 
+    def update
+        @post = current_user.posts.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render :new
+        end
+    end
+
     private
     def post_params
         params.require(:post).permit(:name, :content, :price, :image).merge(user_id: current_user.id)
