247e53f09808063752f4592b0c94e4f12e216012
taiki yamashita <taiki0808ty@gmail.com>
2021-06-08 11:53:57 +0900

    feat: add controller routing setting

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,11 @@
+class ReviewsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
-  resources :posts
+  resources :posts do
+    resources :reviews, only: [:index, :create]
+  end
 
   devise_scope :user do
     post '/users/guest_sign_in', to: 'users/sessions#new_guest'
