13145c71ed186e05454c76f47fb4e4c7e02c6501
taiki yamashita <taiki0808ty@gmail.com>
2021-06-09 10:10:53 +0900

    feat: review index

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,20 @@ class Post < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     has_many :reviews, dependent: :destroy
+
+    def avg_score
+        unless self.reviews.empty?
+            reviews.average(:score).round(1).to_f
+        else
+            0.0
+        end
+    end
+
+    def review_score_percentage
+        unless self.reviews.empty?
+            reviews.average(:score).round(1).to_f*100/5
+        else
+            0.0
+        end
+    end
 end
