e7a154ca8ceb85a5a7e84098f23742b3c5e0436e
taiki yamashita <taiki0808ty@gmail.com>
2021-06-09 11:12:43 +0900

    update: review index

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,17 +21,17 @@ class Post < ApplicationRecord
 
     def avg_score
         unless self.reviews.empty?
-            reviews.average(:score).round(1).to_f
+          reviews.average(:score).round(1).to_f
         else
-            0.0
+          0.0
         end
-    end
-
-    def review_score_percentage
+      end
+    
+      def review_score_percentage
         unless self.reviews.empty?
-            reviews.average(:score).round(1).to_f*100/5
+          reviews.average(:score).round(1).to_f*100/5
         else
-            0.0
+          0.0
         end
-    end
+      end
 end
