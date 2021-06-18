b108508a7a67b8b55dfaf14714c0af1c8f134589
taiki yamashita <taiki0808ty@gmail.com>
2021-06-18 23:20:22 +0900

    update: notification view

--- app/models/post.rb
+++ app/models/post.rb
@@ -96,7 +96,7 @@ class Post < ApplicationRecord
     end
 
     def create_notification_like!(current_user)
-      temp = Notification.where(['visitor_id = ? and visited_id = ? and post_id = ? and action = ? ', current_user.id, user_id, id, 'like'])
+      temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
       if temp.blank?
         notification = current_user.active_notifications.new(
           post_id: id,
