c140c0ee46de1087c228719451424d4137ad3781
taiki yamashita <taiki0808ty@gmail.com>
2021-06-18 10:16:29 +0900

    feat: user notification association

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :entries
   has_many :rooms, through: :entries
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
   
   def display_name
     profile&.nickname || self.username
