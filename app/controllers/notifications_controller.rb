ae0dc3fdbef9c5bf3fd8f0c1413d2f91085f6ac0
taiki yamashita <taiki0808ty@gmail.com>
2021-06-18 12:14:15 +0900

    feat notification view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,4 @@
-class NotificationController < ApplicationController
+class NotificationsController < ApplicationController
     def index
         @notifications = current_user.passive_notifications
         @notifications.where(checked: false).each do |notification|
