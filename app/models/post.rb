a32b439487c2b65e905c99d7cbd5cf38c88d3507
taiki yamashita <taiki0808ty@gmail.com>
2021-06-18 17:49:49 +0900

    fix: like error

--- app/models/post.rb
+++ app/models/post.rb
@@ -95,4 +95,19 @@ class Post < ApplicationRecord
       notification.save if notification.valid?
     end
 
+    def create_notification_like!(current_user)
+      temp = Notification.where(['visitor_id = ? and visited_id = ? and post_id = ? and action = ? ', current_user.id, user_id, id, 'like'])
+      if temp.blank?
+        notification = current_user.active_notifications.new(
+          post_id: id,
+          visited_id: user_id,
+          action: 'like'
+        )
+        if notification.visitor_id == notification.visited_id
+          notification.checked = true
+        end
+        notification.save if notification.valid?
+      end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -92,21 +92,6 @@ class User < ApplicationRecord
     likes.exists?(post_id: post.id)
   end
 
-  def create_notification_like!(current_user)
-    temp = Notification.where(['visitor_id = ? and visited_id = ? and post_id = ? and action = ? ', current_user.id, user_id, id, 'like'])
-    if temp.blank?
-      notification = current_user.active_notifications.new(
-        post_id: id,
-        visited_id: user_id,
-        action: 'like'
-      )
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
-      notification.save if notification.valid?
-    end
-  end
-
   def create_notification_follow!(current_user)
     temp = Notification.where(['visitor_id = ? and visited_id = ? and action = ? ', current_user.id, id, 'follow'])
     if temp.blank?
