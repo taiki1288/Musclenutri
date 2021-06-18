1acb32973c11d7a1f52c4e960e77430cc7b8e625
taiki yamashita <taiki0808ty@gmail.com>
2021-06-18 11:16:11 +0900

    feat: review notification method

--- app/models/post.rb
+++ app/models/post.rb
@@ -74,4 +74,25 @@ class Post < ApplicationRecord
       Post.find(Review.group(:post_id).order('avg(score) desc').limit(4).pluck(:post_id))
     end
 
+    def create_notification_review!(current_user, review_id)
+      temp_ids = Review.select(:user_id).where(post_id: id).where.not(user_id: current_user.id).distinct
+      temp_ids.each do |temp_id|
+        save_notification_review!(current_user, review_id, temp_id['user_id'])
+      end
+      save_notification_review!(current_user, review_id, user_id) if temp_ids.blank?
+    end
+
+    def save_notification_review!(current_user, review_id, visited_id)
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        review_id: review_id,
+        visited_id: visited_id,
+        action: 'review'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+
 end
