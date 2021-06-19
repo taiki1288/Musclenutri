ad5a9197a531d22382373f7126f1259e996bc028
taiki yamashita <taiki0808ty@gmail.com>
2021-06-19 18:56:39 +0900

    update: pagenation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
-      root_path # ログイン後に遷移するpathを設定
+      root_path
     end
     
     def after_sign_out_path_for(resource)
-      new_user_session_path # ログアウト後に遷移するpathを設定
+      new_user_session_path
     end
 
     def check_guest
