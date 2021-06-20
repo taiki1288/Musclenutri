e12607e6c1f75c186dfebe68ed7abad7a3c1af1e
taiki yamashita <taiki0808ty@gmail.com>
2021-06-20 09:53:11 +0900

    update: add sportingevent column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_231145) do
+ActiveRecord::Schema.define(version: 2021_06_20_004727) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_231145) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "sportingevent"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
