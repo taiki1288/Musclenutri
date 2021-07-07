709dde5d9b84caec255668ff9aae1b40d0293f35
taiki yamashita <taiki0808ty@gmail.com>
2021-07-07 14:36:17 +0900

    fix: goods_controller setting

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,13 @@
 class GoodsController < ApplicationController
   def search
     if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      items = SearchForm.new(keyword: params[:keyword])
+      if items.valid?
+        @items = RakutenWebService::Ichiba::Item.search(keyword: items.keyword)
+      else
+        flash.now[:alert] = '入力に誤りがあります。'
+        render 'search'
+      end
     end
   end
 end
