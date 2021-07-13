d55a49144c81cecea8d3ccf022d0c61f7beeb296
taiki yamashita <taiki0808ty@gmail.com>
2021-07-14 00:24:52 +0900

    refactor: css

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,7 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 #
 # port ENV.fetch('PORT') { 3000 }
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-
+# bind "unix:/var/www/musclenutri_app/shared/tmp/sockets/puma.sock"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
