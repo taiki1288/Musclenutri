28d727b2a0124175097919483cfc53ce3f22bf54
taiki yamashita <taiki0808ty@gmail.com>
2021-07-15 01:21:43 +0900

    fix: errorのため元通りに

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-    c.application_id = Rails.application.credentials.rakuten[:api_key]
-    # c.affiliate_id = ENV['RWS_AFFILIATION_ID']
+    c.application_id = ENV['RWS_APPLICATION_ID']
+    c.affiliate_id = ENV['RWS_AFFILIATION_ID']
 end
