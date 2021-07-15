fc769812fbae206a423ad2b6176de5ad1ea409be
taiki yamashita <taiki0808ty@gmail.com>
2021-07-15 17:16:44 +0900

    fix: rakuten product search

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-    c.application_id = ENV['RWS_APPLICATION_ID']
-    c.affiliate_id = ENV['RWS_AFFILIATION_ID']
+    c.application_id = '1051524693967120378'
+    # c.affiliate_id = ENV['RWS_AFFILIATION_ID']
 end
