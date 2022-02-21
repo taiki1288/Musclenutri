bd5fe75366d95fb6c87a645ba622847b8ec5dd86
taiki yamashita <taikity0808@gmail.com>
2022-02-22 00:38:00 +0900

    update: 通知機能のN+1問題を回避

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @users = User.all.includes([:profile]).page(params[:page])
+        @users = User.includes([:profile]).page(params[:page])
     end
 
     def show
